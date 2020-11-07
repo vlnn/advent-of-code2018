6968fec03f8f6edfd329abd05a11a8ddc0992604
vlnn <anohin.va@gmail.com>
2020-11-07 22:48:18 +0200

    Remove merge markers

--- Advent2018.Tests/UnitTest1.cs
+++ Advent2018.Tests/UnitTest1.cs
@@ -1,19 +1,5 @@
 using NUnit.Framework;
 
-<<<<<<< HEAD
-namespace Advent2018.Tests
-{
-    public class Tests
-    {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
-        [Test]
-        public void Test1()
-        {
-=======
 namespace Advent2018.Tests {
     public class Tests {
         [SetUp]
@@ -21,7 +7,6 @@ namespace Advent2018.Tests {
 
         [Test]
         public void Test1() {
->>>>>>> 6d43e203e85e41b3b03e7460b2b569aa52aa94db
             Assert.Pass();
         }
     }
